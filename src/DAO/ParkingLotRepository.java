00a66136bcdd3a5351770566604b2a34085e4674
Vaishali <missbaishali11@gmail.com>
2026-07-11 15:32:07 +0530

    Add Repository layer for parking operations

--- src/DAO/ParkingLotRepository.java
+++ src/DAO/ParkingLotRepository.java
@@ -1,4 +1,16 @@
 package DAO;
 
+import DTO.ParkingLot;
+
 public class ParkingLotRepository {
+
+    private ParkingLot parkingLot;
+
+    public void saveParkingLot(ParkingLot parkingLot){
+        this.parkingLot = parkingLot;
+    }
+
+    public ParkingLot getParkingLot(){
+        return parkingLot;
+    }
 }
